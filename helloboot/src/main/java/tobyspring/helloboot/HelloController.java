b2a0854364d934481185da0421099301858a6644
yujin <sdyj178@gmail.com>
2023-02-16 22:49:15 +0900

    20230216-done-spring-#3: front controller (mapping & binding)

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,10 +3,10 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class HelloController {
-//    @GetMapping("/hello")
-//    public String hello(String name) {
-//        return "Hello " + name;
-//    }
+    //    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
 }
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,6 +5,10 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -16,25 +20,35 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        // 설치하고 기본 설정 정보들이 있는데 스프링 부트가 이를 간단하게 만들어주는 클래스가 있다.
-        // new Tomcat().start(); 요것만으로는 부족하다
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // 일종의 도우미 클래스
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("hello", new HttpServlet() {
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("frontcontroller", new HttpServlet() {
+                    HelloController helloController = new HelloController();
+
                     @Override
                     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        // 상태 라인에서 상태 코드
-                        resp.setStatus(200);
-                        // 헤드들을 만들고 그 중에서 바디 타입이 꼭 포함
-                        resp.setHeader("Content-Type", "text/plain");
-                        // 바디 내용
-                        resp.getWriter().println("hello Servlet");
-                    }
-                }).addMapping("/hello")
-        ); // 추상화를 했기에 TomcatServletWebServerFactory이 아니라 ServletWebServerFactory으로 받아도 문제 없음
-        webServer.start(); //이걸 실행하면 톰캣 서블릿 컨테이너가 동작함
+                        // 모든 요청이 들어온다.
+                        // 인증, 보안, 다국어 처리, 공통 기능들을 앞에서 한다면?
+                        // 이후에는 무엇을 해야하나? -> 서블릿 컨트롤러의 매핑기능을 프론트 컨트롤러가 대신한다.
+                        // 매칭은 요청을 가지고 처리한다. 메소드, path, url, header, body 그 중 대표는 url
 
-        // 서블릿을 컨테너에 추가하는 방법
+                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                            String name = req.getParameter("name");
+                            String ret = helloController.hello(name);
+
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.getWriter().println(ret); // 받은 쿼리 파라미터를 이용하여 출력함
+                        } else if (req.getRequestURI().equals("/user")) {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                            resp.getWriter().println("NOT FOUND");
+                        }
+                    }
+                }).addMapping("/*") // /밑으로 들어오는 모든 요청을 처리하겠다.
+        );
+        webServer.start();
+
 
     }
 
