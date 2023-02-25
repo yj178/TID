d867c0c5d914e8dc4a01739fa311182598d83f3f
yujin <sdyj178@gmail.com>
2023-02-25 21:23:59 +0900

    20230225-done-spring-#3: SpringBoot처럼 만들기 전

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,16 +1,23 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
 //@RestController // 해당 어노테이션은 DispatcherServlet과 직접적인 연관은 없음
+@MyComponent
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext; // final로 만들면 안되는 이유 생성자 완료된 이후에 호출되기 때문에
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
 
@@ -21,4 +28,6 @@ public class HelloController {
 
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+
 }
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,51 +1,36 @@
 package tobyspring.helloboot;
 
-
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.swing.*;
-import java.io.IOException;
+
+@Configuration
+@ComponentScan // 해당 패키지부터 하위의 모든 패키지에 존재하는 모든 Component를 등록한다.
+// 등록되는 Bean이 매우 많다면 번거로울 수 있다.
+// 클래스를 모두 뒤져서 컴포넌트 붙은데 이것 저것 있으니까 확인하고자 할 때 모두 찾아보기 힘들다.
+// 패키지와 모듈을 잘 활용해서 나누면 찾아보기 편하다.
 
 public class HellobootApplication {
 
-    public static void main(String[] args) {
-        // Spring Container를 만드는 코드
-        // dispatcher servlet은 웹 환경에서 사용하는 GenericWebApplicationContext로 변경해야함
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
-
-        // Servlet Container를 코드로 동작하면서 등록하는 작업
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            // 웹 어플리케이션
-            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) {
-
-
-            }).addMapping("/*");// /밑으로 들어오는 모든 요청을 처리하겠다.
-        });
-        webServer.start();
-
-
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public static void main(String[] args) {
+        MySpringApplication.run(HellobootApplication.class, args);
+    }
+
 
 }
