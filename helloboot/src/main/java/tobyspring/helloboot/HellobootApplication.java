4d1e50fc36a3af044785b749851a5dd86ec728fe
yujin <sdyj178@gmail.com>
2023-02-25 21:27:18 +0900

    20230225-done-spring-#3: 섹션 04. 독립 실행형 스프링 어플리케이션 완료

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 
 @Configuration
-@ComponentScan // 해당 패키지부터 하위의 모든 패키지에 존재하는 모든 Component를 등록한다.
-// 등록되는 Bean이 매우 많다면 번거로울 수 있다.
-// 클래스를 모두 뒤져서 컴포넌트 붙은데 이것 저것 있으니까 확인하고자 할 때 모두 찾아보기 힘들다.
-// 패키지와 모듈을 잘 활용해서 나누면 찾아보기 편하다.
-
+@ComponentScan
 public class HellobootApplication {
 
     @Bean
@@ -29,7 +26,7 @@ public class HellobootApplication {
     }
 
     public static void main(String[] args) {
-        MySpringApplication.run(HellobootApplication.class, args);
+        SpringApplication.run(HellobootApplication.class, args);
     }
 
 
