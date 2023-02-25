5a30456306b79addfe7f62973cb558b7a29639f2
yujin <sdyj178@gmail.com>
2023-02-25 17:02:17 +0900

    20230225-done-spring-#3: Spring Container 의존 오브젝트 추가

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,9 +2,12 @@ package tobyspring.helloboot;
 
 
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -15,31 +18,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // 스프링 컨테이너 만들기
+        // 애플리케이션을 구성하고 있는 많은 작업들을 구현하는 부분을
+        // 코드로 쉽게 만들 수 있게 도와주는 클래스
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        // 오브젝트를 직접 만들어서 넣는것도 가능하지만 어떤 클래스를 활용해서 bean 오브젝트를 만들것인지 등록한다.
+        // 클래스 정보만 넘김
+        applicationContext.registerBean(HelloController.class); // 등록 완료
+        applicationContext.refresh(); // bean 오브젝트를 전부 생성해줌
+
+
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("frontcontroller", new HttpServlet() {
-                    HelloController helloController = new HelloController();
 
                     @Override
                     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        // 모든 요청이 들어온다.
-                        // 인증, 보안, 다국어 처리, 공통 기능들을 앞에서 한다면?
-                        // 이후에는 무엇을 해야하나? -> 서블릿 컨트롤러의 매핑기능을 프론트 컨트롤러가 대신한다.
-                        // 매칭은 요청을 가지고 처리한다. 메소드, path, url, header, body 그 중 대표는 url
+
 
                         if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                             String name = req.getParameter("name");
+
+                            // 이름을 직접 지정할 수도 있고, 클래스 타입을 지정할 수도 있다.
+                            HelloController helloController = applicationContext.getBean(HelloController.class);
                             String ret = helloController.hello(name);
 
                             resp.setStatus(HttpStatus.OK.value());
-                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                             resp.getWriter().println(ret); // 받은 쿼리 파라미터를 이용하여 출력함
-                        } else if (req.getRequestURI().equals("/user")) {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
                         } else {
                             resp.setStatus(HttpStatus.NOT_FOUND.value());
                             resp.getWriter().println("NOT FOUND");
