ef000c89a0f28e01c644b6e6b1ba09bad4b41fd6
yujin <sdyj178@gmail.com>
2023-02-25 17:29:09 +0900

    20230225-done-spring-#3: DispatcherServlet 적용 전

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -31,34 +31,36 @@ public class HellobootApplication {
         // 오브젝트를 직접 만들어서 넣는것도 가능하지만 어떤 클래스를 활용해서 bean 오브젝트를 만들것인지 등록한다.
         // 클래스 정보만 넘김
         applicationContext.registerBean(HelloController.class); // 등록 완료
+        applicationContext.registerBean(SimpleHelloService.class); // 등록 완료
         applicationContext.refresh(); // bean 오브젝트를 전부 생성해줌
 
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("frontcontroller", new HttpServlet() {
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
 
-                    @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = req.getParameter("name");
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
 
-                            // 이름을 직접 지정할 수도 있고, 클래스 타입을 지정할 수도 있다.
-                            HelloController helloController = applicationContext.getBean(HelloController.class);
-                            String ret = helloController.hello(name);
+                        // 이름을 직접 지정할 수도 있고, 클래스 타입을 지정할 수도 있다.
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
+                        String ret = helloController.hello(name);
 
-                            resp.setStatus(HttpStatus.OK.value());
+                        resp.setStatus(HttpStatus.OK.value());
 //                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            resp.getWriter().println(ret); // 받은 쿼리 파라미터를 이용하여 출력함
-                        } else {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
-                            resp.getWriter().println("NOT FOUND");
-                        }
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret); // 받은 쿼리 파라미터를 이용하여 출력함
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        resp.getWriter().println("NOT FOUND");
                     }
-                }).addMapping("/*") // /밑으로 들어오는 모든 요청을 처리하겠다.
-        );
+                }
+            }).addMapping("/*");// /밑으로 들어오는 모든 요청을 처리하겠다.
+        });
         webServer.start();
 
 
