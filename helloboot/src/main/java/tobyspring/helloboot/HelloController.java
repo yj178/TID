69fe5a5b1293ab09ba61812a8de16f9d2f65c7b3
yujin <sdyj178@gmail.com>
2023-02-25 20:19:41 +0900

    20230225-done-spring-#3: DispatcherServlet 적용 후

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,10 +1,23 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Objects;
 
+//@RestController // 해당 어노테이션은 DispatcherServlet과 직접적인 연관은 없음
+@RequestMapping("/hello")
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+
+    @GetMapping
+    @ResponseBody
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
 
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -24,41 +26,21 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        // 스프링 컨테이너 만들기
-        // 애플리케이션을 구성하고 있는 많은 작업들을 구현하는 부분을
-        // 코드로 쉽게 만들 수 있게 도와주는 클래스
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
-        // 오브젝트를 직접 만들어서 넣는것도 가능하지만 어떤 클래스를 활용해서 bean 오브젝트를 만들것인지 등록한다.
-        // 클래스 정보만 넘김
-        applicationContext.registerBean(HelloController.class); // 등록 완료
-        applicationContext.registerBean(SimpleHelloService.class); // 등록 완료
-        applicationContext.refresh(); // bean 오브젝트를 전부 생성해줌
+        // Spring Container를 만드는 코드
+        // dispatcher servlet은 웹 환경에서 사용하는 GenericWebApplicationContext로 변경해야함
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh();
 
+        // Servlet Container를 코드로 동작하면서 등록하는 작업
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            // 웹 어플리케이션
+            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) {
 
 
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        // 이름을 직접 지정할 수도 있고, 클래스 타입을 지정할 수도 있다.
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setStatus(HttpStatus.OK.value());
-//                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret); // 받은 쿼리 파라미터를 이용하여 출력함
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                        resp.getWriter().println("NOT FOUND");
-                    }
-                }
             }).addMapping("/*");// /밑으로 들어오는 모든 요청을 처리하겠다.
         });
         webServer.start();
